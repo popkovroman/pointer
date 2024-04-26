2ba5eb7140e5434828eb10ad4f80301f6aa5aece
popkovroman <romka200901@mail.ru>
2024-04-26 15:46:08 +0700

    fix

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -4,9 +4,9 @@ int main() {
     int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int* pointer = arr;
 
-    int* pinterfor4 = pointer + 3;
+    int* pointerfor4 = pointer + 3;
 
-    std::cout << "Elem 4:" << *pinterfor4 << "\n";
+    std::cout << "Elem 4:" << *pointerfor4 << "\n";
 
     return 0;
 }
